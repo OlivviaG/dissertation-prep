786ff9da3448a180623c898c6321359d4606dc94
Oliwia <oliwiagrochowska02@gmail.com>
2026-05-28 15:05:38 +0100

    Add Energy score and update CSV (with energy)

--- src/logger.py
+++ src/logger.py
@@ -6,23 +6,26 @@ from datetime import datetime
 
 # mood tracker logger
 def get_mood():
-<<<<<<< HEAD
     mood = input("Mood score (1-10): ")
-=======
-    mood = input("Rate your mood (1-10): ")
->>>>>>> feature/energy-input
     return int(mood)
 
-def save_entry(mood):
+def get_energy():
+    energy = input("Energy score (1-10): ")
+    return int(energy)
+
+def save_entry(mood, energy):
+
     filename = "data/checkin.csv"           # create a filename "checkin.csv" in the "data" folder
     file_exists = os.path.isfile(filename)  # if data folder doesn't exist make it with os.makedirs
 
     with open(filename, "a", newline="") as f: 
         writer = csv.writer(f)
         if not file_exists: # if the file doesn't exist, write the header
-            writer.writerow(["timestamp", "mood"])
-        writer.writerow([datetime.now().strftime("%Y-%m-%d %H:%M:%S"), mood]) # write the timestamp and mood to the csv file
+            writer.writerow(["timestamp", "mood", "energy"])
+        writer.writerow([datetime.now().strftime("%Y-%m-%d %H:%M:%S"), mood, energy]) # write the timestamp, mood, and energy to the csv file
 
     if __name__ == "__main__":
         mood = get_mood()           # get user input for mood
-        save_entry(mood)            # save the mood entry to the csv file
+        energy = get_energy()       # get user input for energy
+        save_entry(mood, energy)    # save the entry to the csv file
+        print(f"Entry saved: Mood={mood}, Energy={energy}")
