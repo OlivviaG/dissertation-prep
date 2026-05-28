192e25cbfbbf5449a77fc812a16b65fef9b34e23
Oliwia <oliwiagrochowska02@gmail.com>
2026-05-28 14:11:48 +0100

    Add mood input and CSV saving to logger

---
+++ src/logger.py
@@ -0,0 +1,22 @@
+import csv
+from fileinput import filename
+import os
+from datetime import datetime
+
+
+# mood tracker logger
+def get_mood():
+    mood = input("How are you feeling today? (1-10): ")
+    return int(mood)
+
+def save_entry(mood):
+    filename = "data/checkin.csv"           # create a filename "checkin.csv" in the "data" folder
+    file_exists = os.path.isfile(filename)  # if data folder doesn't exist make it with os.makedirs
+
+    with open(filename, "a", newline="") as f: 
+        writer = csv.writer(f)
+        if not file_exists: # if the file doesn't exist, write the header
+            writer.writerow(["timestamp", "mood"])
+        writer.writerow([datetime.now().strftime("%Y-%m-%d %H:%M:%S"), mood]) # write the timestamp and mood to the csv file
+
+    
