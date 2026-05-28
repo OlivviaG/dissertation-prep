1d6dad985b36c47873948b1042c45a220c7c62ae
Oliwia <oliwiagrochowska02@gmail.com>
2026-05-28 15:08:25 +0100

    Add free-text note input and update CSV columns

--- src/logger.py
+++ src/logger.py
@@ -13,15 +13,21 @@ def get_energy():
     energy = input("Energy score (1-10): ")
     return int(energy)
 
-def save_entry(mood, energy):
+def get_note():
+    note = input("Any notes for today (press enter to skip): ")
+    return note
+
+
+def save_entry(mood, energy, note):
 
     filename = "data/checkin.csv"           # create a filename "checkin.csv" in the "data" folder
     file_exists = os.path.isfile(filename)  # if data folder doesn't exist make it with os.makedirs
 
+
     with open(filename, "a", newline="") as f: 
         writer = csv.writer(f)
         if not file_exists: # if the file doesn't exist, write the header
-            writer.writerow(["timestamp", "mood", "energy"])
+            writer.writerow(["timestamp", "mood", "energy", "note"]) # write the header to the csv file 
         writer.writerow([datetime.now().strftime("%Y-%m-%d %H:%M:%S"), mood, energy]) # write the timestamp, mood, and energy to the csv file
 
     if __name__ == "__main__":
